dfac619ab470763a543d754844e1bb8793d641dc
Oscar Mendez <oscar.mendez@avaldigitallabs.com>
2021-03-16 19:37:12 -0500

    :sparkles: Initial config

---
+++ src/main/java/edu/escuelaing/taskplanner/backend/TaskplannerBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.taskplanner.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskplannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskplannerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/escuelaing/taskplanner/backend/TaskplannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.taskplanner.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskplannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
